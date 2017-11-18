374dadd164ae4d8f424ad2fa61451d515f333963
tim <tkovac45@gmail.com>
2017-11-18 17:36:00 +0100

    DODANO: Zaslon predmeta

---
+++ FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/CommentDialog.java
@@ -0,0 +1,97 @@
+package si.lj.uni.fri.tpo.fripredmeti;
+
+/**
+ * Created by timko on 18. 11. 2017.
+ */
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.media.Image;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by timko on 6. 09. 2017.
+ */
+
+public class CommentDialog  implements DialogInterface.OnDismissListener{
+    private Activity mActivity;
+
+    public CommentDialog(Activity a)
+    {
+        mActivity = a;
+    }
+
+    public Dialog showDialog(final Activity activity){
+        final Dialog dialog = new Dialog(activity);
+        mActivity = activity;
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.comment_dialog);
+
+        CardView cv = (CardView) dialog.findViewById(R.id.card_more);
+        cv.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ImageView tv = (ImageView) v.findViewById(R.id.view_more);
+                LinearLayout ll = (LinearLayout) v.getParent();
+                RelativeLayout rl = (RelativeLayout) ll.getChildAt(0);
+                if(rl.getVisibility() == View.GONE)
+                {
+                    rl.setVisibility(View.VISIBLE);
+                    tv.setBackgroundResource(R.drawable.ic_expand_less_black_24dp);
+                }
+                else
+                {
+                    rl.setVisibility(View.GONE);
+                    tv.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                }
+            }
+        });
+
+        dialog.show();
+        return dialog;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {}
+}
--- FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/MainActivity.java
+++ FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/MainActivity.java
@@ -1,7 +1,17 @@
 package si.lj.uni.fri.tpo.fripredmeti;
 
+import android.graphics.Rect;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Računalniška grafika");
+
+        ScrollView sv = (ScrollView) findViewById(R.id.sw);
+        sv.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                Rect scrollBounds = new Rect();
+                v.getHitRect(scrollBounds);
+                FrameLayout tv15 = (FrameLayout) findViewById(R.id.fl2);
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+                if (tv15.getLocalVisibleRect(scrollBounds)) {
+                    fab.animate().translationY(0).setInterpolator(new LinearInterpolator()).start();
+                } else {
+                    FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab.getLayoutParams();
+                    int fab_bottomMargin = layoutParams.bottomMargin;
+                    fab.animate().translationY(fab.getHeight() + fab_bottomMargin).setInterpolator(new LinearInterpolator()).start();
+                }
+            }
+        });
+
+    }
+
+
+    public void showDialog(View v)
+    {
+        CommentDialog cd = new CommentDialog(this);
+        cd.showDialog(this);
     }
 }
