575db7a2368dfe00c02a24cb4c5f1849f00cfa1d
tim <tkovac45@gmail.com>
2017-11-19 15:11:42 +0100

    DODANO: zaslon + search

--- FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/MainActivity.java
+++ FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/MainActivity.java
@@ -3,10 +3,14 @@ package si.lj.uni.fri.tpo.fripredmeti;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.FrameLayout;
@@ -15,14 +19,16 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  SearchView.OnQueryTextListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getSupportActionBar().setTitle("Področja");
+        getSupportActionBar().setTitle("Grafika in umetna inteligenca");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
     }
 
 
@@ -32,4 +38,30 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(this);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onQueryTextChange(String newText)
+    {
+        if (TextUtils.isEmpty(newText))
+        {
+
+        }
+        else
+        {
+
+        }
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
 }
