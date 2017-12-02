8327c72491aa6a9340c9a93da44661d90af4636f
Simon Tušar <SIMON.TUSAR@GMAIL.COM>
2017-12-02 19:20:01 +0100

    Dodana osnova podatkovnega strežnika

---
+++ Server/FriAplikacija/FriAplikacija/IService.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FriAplikacija
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "Login", ResponseFormat = WebMessageFormat.Json)]
+        String Login();
+    }
+}
