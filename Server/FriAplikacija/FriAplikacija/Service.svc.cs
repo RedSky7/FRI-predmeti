fa6b8b28a224bfb9b39e27cc23ad539424b6e45b
Klemen <ExPie@users.noreply.github.com>
2017-12-08 23:26:39 +0100

    Test pushing

--- Server/FriAplikacija/FriAplikacija/Service.svc.cs
+++ Server/FriAplikacija/FriAplikacija/Service.svc.cs
@@ -10,11 +10,9 @@ namespace FriAplikacija
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service.svc or Service.svc.cs at the Solution Explorer and start debugging.
-    public class Service : IService
-    {
-        String IService.Login()
-        {
-            return "HejHithoAdi";
+    public class Service : IService {
+        String IService.Login() {
+            return "This was not meant for you, Dave.";
         }
     }
 }
