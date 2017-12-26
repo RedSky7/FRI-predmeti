cf1b76dc48b023e7df448627a5eb7ec953e84017
tim <tkovac45@gmail.com>
2017-12-26 19:58:33 +0100

    ADDED: simple animacija, recyclerView

--- FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/ClassOverview.java
+++ FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/ClassOverview.java
@@ -3,25 +3,49 @@ package si.lj.uni.fri.tpo.fripredmeti;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class ClassOverview extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_overview);
 
+
+
+
         //setSupportActionBar((Toolbar) findViewById(R.id.toolbar2));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setTitle("Računalniška grafika");
+
+        setTitle(getIntent().getStringExtra("title"));
 
         ScrollView sv = (ScrollView) findViewById(R.id.sw);
         sv.setOnScrollChangeListener(new View.OnScrollChangeListener() {
@@ -32,15 +56,19 @@ public class ClassOverview extends AppCompatActivity {
                 FrameLayout tv15 = (FrameLayout) findViewById(R.id.fl2);
                 FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
                 if (tv15.getLocalVisibleRect(scrollBounds)) {
-                    fab.animate().translationY(0).setInterpolator(new LinearInterpolator()).start();
+                    fab.animate().translationY(0).setInterpolator(new AccelerateInterpolator()).start();
                 } else {
                     RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) fab.getLayoutParams();
                     int fab_bottomMargin = layoutParams.bottomMargin;
-                    fab.animate().translationY(fab.getHeight() + fab_bottomMargin).setInterpolator(new LinearInterpolator()).start();
+                    fab.animate().translationY(fab.getHeight() + fab_bottomMargin).setInterpolator(new AccelerateInterpolator()).start();
                 }
             }
         });
+
+
     }
+
+
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -60,5 +88,7 @@ public class ClassOverview extends AppCompatActivity {
         Intent intent = new Intent(this, TeacherOverview.class);
         startActivity(intent);
     }
+
+
 
 }
--- FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/MainActivity.java
+++ FRIpredmeti/app/src/main/java/si/lj/uni/fri/tpo/fripredmeti/MainActivity.java
@@ -1,25 +1,37 @@
 package si.lj.uni.fri.tpo.fripredmeti;
 
 import android.content.Intent;
-import android.graphics.Rect;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentTransaction;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
-import android.widget.FrameLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements  SearchView.OnQueryTextListener {
+import si.lj.uni.fri.tpo.fripredmeti.Fragments.ClassessFragment;
+import si.lj.uni.fri.tpo.fripredmeti.Fragments.TeachersFragment;
+
+public class MainActivity extends AppCompatActivity implements  ClassessFragment.OnFragmentInteractionListener, TeachersFragment.OnFragmentInteractionListener, SearchView.OnQueryTextListener {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    public ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,29 @@ public class MainActivity extends AppCompatActivity implements  SearchView.OnQue
         getSupportActionBar().setTitle("Strojna oprema");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
+        //imageView.animate().rotationY(180).setInterpolator(new LinearInterpolator()).start();
+        //imageView.animate().rotationYBy(180).setInterpolator(new LinearInterpolator()).start();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new ClassessFragment(), "predmeti");
+        adapter.addFragment(new TeachersFragment(), "izvajalci");
+
+        viewPager.setAdapter(adapter);
+
+        viewPager.setCurrentItem(0);
     }
 
 
@@ -43,6 +78,13 @@ public class MainActivity extends AppCompatActivity implements  SearchView.OnQue
         Intent intent = new Intent(this, ClassOverview.class);
         startActivity(intent);
     }
+
+    public void startTeacherOverview(View v)
+    {
+        Intent intent = new Intent(this, TeacherOverview.class);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -76,4 +118,77 @@ public class MainActivity extends AppCompatActivity implements  SearchView.OnQue
     public boolean onQueryTextSubmit(String query) {
         return false;
     }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
+
+
+        @Override
+        public int getItemPosition(Object obj)
+        {
+            return POSITION_NONE;
+        }
+
+
+
+        @Override
+        public void startUpdate(ViewGroup container) {
+            super.startUpdate(container);
+            /*
+            if(container != null) {
+                for(int i = 0; i < container.getChildCount(); i++)
+                {
+                    View view = container.getChildAt(i);
+
+                    RecyclerView rv = (RecyclerView) view.findViewById(R.id.recycler_view);
+
+                    boolean notEmpty = rv.getAdapter().getItemCount() == 0;
+
+                    if (notEmpty) {
+                        ImageView iw = (ImageView) view.findViewById(R.id.fs_emptyIcon);
+                        TextView tw = (TextView) view.findViewById(R.id.fs_emptyText);
+                        if (iw != null && tw != null) {
+                            Log.e("HIDING2", notEmpty + "");
+                            iw.setVisibility((notEmpty) ? View.VISIBLE : View.GONE);
+                            tw.setVisibility((notEmpty) ? View.VISIBLE : View.GONE);
+                        }
+                    }
+
+
+                }
+            }*/
+        }
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){}
 }
