bbeb2ea23f19e2a2a237b64e23fc00a8c26772c2
Simon Tušar <SIMON.TUSAR@GMAIL.COM>
2018-01-06 13:08:07 +0100

    Added check if email is allredy registered

--- Server/FriAplikacija/FriAplikacija/Service.svc.cs
+++ Server/FriAplikacija/FriAplikacija/Service.svc.cs
@@ -14,6 +14,11 @@ namespace FriAplikacija
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service.svc or Service.svc.cs at the Solution Explorer and start debugging.
     public class Service : IService {
+        public Boolean CheckEmail() {
+            WebOperationContext ctx = WebOperationContext.Current;
+            String email = ctx.IncomingRequest.Headers["email"].ToString();
+            return UporabinkDataAccess.getEmail(email);
+        }
         public Uporabnik Login() {
             WebOperationContext ctx = WebOperationContext.Current;
             String email = ctx.IncomingRequest.Headers["email"].ToString();
